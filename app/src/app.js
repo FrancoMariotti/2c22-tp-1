566bbe4c801f96b93415d649d67682b0bb36aebc
jpdicomo <39575121+jpdicomo@users.noreply.github.com>
2022-10-04 17:12:03 -0300

    Added intensive endpoint

--- app/src/app.js
+++ app/src/app.js
@@ -1,12 +1,26 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+const { fibonacci } = require("./intensive");
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get("/", function (req, res) {
+  console.log("PING");
+  res.status(200).send("PING");
+});
+
+//Intensive
+app.get("/fibonacci", function (req, res) {
+  console.log("FIBONACCI");
+  var fib;
+  if (req.query.n === undefined) {
+    fib = fibonacci(45);
+  } else {
+    fib = fibonacci(parseInt(req.query.n));
+  }
+  res.send(fib);
 });
 
 app.listen(PORT, HOST, () => {
