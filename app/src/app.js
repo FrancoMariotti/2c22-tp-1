da2a60e607ad54f3123ba53c3aded4e295a9c19b
jpdicomo <jdicomo@fi.uba.ar>
2022-10-07 20:35:52 -0300

    added async and sync endpoints

--- app/src/app.js
+++ app/src/app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const axios = require("axios");
 var app = express();
 const { fibonacci } = require("./intensive");
 
@@ -21,6 +22,28 @@ app.get("/fibonacci", function (req, res) {
     fib = fibonacci(parseInt(req.query.n));
   }
   res.send(fib);
+});
+
+app.get("/sync", (req, res) => {
+  console.log('Bbox 9090 sync');
+  axios.get(`http://bbox:9090/`)
+      .then(result => {
+          res.send(`Bbox port 9090: Status ${result.status} Data ${result.data}`);
+      })
+      .catch(err => {
+          res.send(`Error: ${err.message}`);
+      });
+});
+
+app.get("/async", (req, res) => {
+  console.log('Bbox 9090 async');
+  axios.get(`http://bbox:9091/`)
+      .then(result => {
+          res.send(`Bbox port 9091: Status ${result.status} Data ${result.data}`);
+      })
+      .catch(err => {
+          res.send(`Error: ${err.message}`);
+      });
 });
 
 app.listen(PORT, HOST, () => {
