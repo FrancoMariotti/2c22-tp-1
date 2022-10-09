7ba4d77acac87df6e74be0601cdcc1446ecafbdf
Franco-Mariotti <franco.mariotti@arbe.com.ar>
2022-10-08 22:49:13 -0300

    minor changes

--- app/src/app.js
+++ app/src/app.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var app = express();
 
-// Constants
-const PORT = 3000;
-const HOST = '0.0.0.0';
+const port = process.env.PORT || 5000;
 
 app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
